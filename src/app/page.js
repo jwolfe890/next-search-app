db37aa9e366553f32535820fa959cc8b3a6e17d1
John Wolfe <jwolfe890@gmail.com>
2025-02-28 00:36:10 -0600

    update

--- src/app/page.js
+++ src/app/page.js
@@ -3,11 +3,15 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
+      <p>
+      Welcome to [Website Name], your premier destination for vintage sports cards! Whether you're a seasoned collector or just starting out, we specialize in rare and iconic trading cards from baseball, football, basketball, and more. Our carefully curated selection features everything from classic rookie cards to hard-to-find gems, ensuring you can add history to your collection. With a passion for preserving the legacy of sports, we provide expert insights, fair pricing, and a seamless shopping experience. Explore our collection today and rediscover the thrill of vintage sports card collecting with [Website Name]!
+      </p>
       <h1>Home Page</h1>
       <Link href="/sets/1950bowman">
         View 1950 Bowman Set
       </Link>
       <Link href="/about">Go to About Page</Link>
+      <Link href="/nonsports/batman">Batman</Link>
     </div>
   );
 }
